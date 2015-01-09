700407c8e9bebedcb9f3d578474910b6f25f977b
Pepe <joseangel.cruzhernandez@gmail.com>
2015-01-08 19:40:23 -0600

    home | acordeon, render y js ajustados

--- web/index.php
+++ web/index.php
@@ -1,3 +1,139 @@
+<!DOCTYPE html>
+<html lang="es">
+    <meta name="charset" content="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+    <meta name="language" content="es" />
+    <meta name="robots" content="index,follow,noarchive" />        
+
+    <title>Home | InventMx</title>
+
+    <link rel="stylesheet" type="text/css" href="css/globals_style.css">
+
+</head>
+<body>
+    <header>
+        <div class="wrapper-header">
+            <div class="wrapper-site-logo left">
+                <a href="/">
+                    <span class="site-invent block"></span>
+                </a>                    
+                <h1 class="none">Inventmx</h1>
+            </div>
+            <ul class="left">
+                <li class="left inline-block">
+                    <a class="none-decoration" href="/">AUDIENCIAS Y CONTENIDOS</a>
+                </li>
+                <li class="left inline-block">
+                    <a class="none-decoration" href="/">MARKETERS</a>
+                </li>
+                <li class="left inline-block">
+                    <a class="none-decoration" href="/">RED DE VIDEO</a>
+                </li>
+                <li class="left inline-block">
+                    <a class="none-decoration" href="/">CASO DE ÉXITO</a>
+                </li>
+                <li class="left inline-block">
+                    <a class="none-decoration" href="/">AFILIATE</a>
+                </li>
+                <li class="left inline-block">
+                    <a class="none-decoration" href="/">ANUNCIATE</a>
+                </li>
+                <span class="clear block"></span>
+            </ul>                
+            <div class="clear"></div>
+        </div>
+    </header>
+
+    <div id="wrapper-page-site"></div>
+
+    <footer>
+        <div class="footer-content-site-invent">
+            <div class="footer-site-invent"></div>
+        </div>
+        <p class="center">
+            Bucareli No. 1 Col. Centro, delegació Cuauhtémoc, C.P. 06000, México
+        </p>
+
+    </footer>    
+
+    <!-- <script data-main="js/site/main" src="js/libs/require.js"></script> -->
+    <!-- <script type="text/javascript" src="js/libs/jquery-1.8.3.min.js"></script> -->
+    <script type="text/javascript" src="js/libs/inventmx_libs.min.js"></script>
+    <!-- <script type="text/javascript" src="js/libs/html5shiv.js"></script>
+    <script type="text/javascript" src="js/libs/underscore-min.js"></script>
+    <script type="text/javascript" src="js/libs/handlebars-v1.3.0.min.js"></script>
+    <script type="text/javascript" src="js/libs/backbone-min.js"></script>     -->
+    <script type="text/javascript" src="js/site/alls.js"></script>
+    <script type="text/javascript" src="js/site/view.js"></script>
+    <script type="text/javascript" src="js/site/routing.js"></script>    
+</body>
+</html>
+
 <?php
-echo "Aqui carga todo lo relacionado al Frontend del sitio";
+ $rutafisica = "data/home/home.json";
+
+  $data_center = array();
+
+  $data_center["data"]["sections"]["0"]["name"] = "RED DE VIDEOS";
+  $data_center["data"]["sections"]["0"]["items"]["0"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["0"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["0"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["0"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["1"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["1"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["1"]["title"] = "Tips de Belleza";
+  $data_center["data"]["sections"]["0"]["items"]["1"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["2"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["2"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["2"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["2"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["3"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["3"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["3"]["title"] = "Tips de Belleza";
+  
+  $data_center["data"]["sections"]["0"]["items"]["4"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["4"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["4"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["4"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["5"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["5"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["5"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["5"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["6"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["6"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["6"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["6"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["7"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["7"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["7"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["7"]["url"] = "/";  
+  
+  $data_center["data"]["sections"]["0"]["items"]["8"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["8"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["8"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["8"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["9"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["9"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["9"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["9"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["10"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["10"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["10"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["10"]["url"] = "/";  
+  $data_center["data"]["sections"]["0"]["items"]["11"]["image"] = "/img/home/global/Talento.png"; 
+  $data_center["data"]["sections"]["0"]["items"]["11"]["name_editor"] = "Ana Dominguez";
+  $data_center["data"]["sections"]["0"]["items"]["11"]["title"] = "Tips de Belleza";  
+  $data_center["data"]["sections"]["0"]["items"]["11"]["url"] = "/";  
+  
+  $data_center["data"]["sections"]["1"]["name"] = "LA RED INVENT";
+  $data_center["data"]["sections"]["1"]["items"]["0"]["image"] = "/";  
+  $data_center["data"]["sections"]["1"]["items"]["0"]["site"] = "Excelsior";  
+  $data_center["data"]["sections"]["1"]["items"]["0"]["sumary"] = "Portal de noticias líder en México con toda la información nacional y del mundo. Entérate en tiempo real de la vida pública, política de espectáculos, deportes, videos y más…"; 
+  $data_center["data"]["sections"]["1"]["items"]["1"]["image"] = "/";  
+  $data_center["data"]["sections"]["1"]["items"]["1"]["site"] = "Imagen Radio";  
+  $data_center["data"]["sections"]["1"]["items"]["1"]["sumary"] = "Sitio web de la estación número 1 de la radio hablada en México. Sitio de noticias, finanza, negocios, deportes, vida, música y curiosidades.";  
+  
+
+  file_put_contents($rutafisica, json_encode($data_center)); 
+?>
 
---
+++ web/js/site/view.js
@@ -0,0 +1,99 @@
+$(function() {
+    /* modelos */
+    inventMx.home.Models = Backbone.Model.extend({});
+    /* colecciones */
+    inventMx.home.Collections = Backbone.Collection.extend({
+        model: inventMx.home.Models,
+        url: "/",
+    });
+
+
+    /* home */
+    inventMx.home.viewHome = Backbone.View.extend({
+        el: inventMx.page.wrapper_site,
+        initialize: function () {
+            $(this.el).unbind();
+            _.bindAll(this, 'render');
+            this.loadPage();
+        },
+        render: function (data) {            
+            //this.loadPage();
+        },
+        loadPage: function () {            
+            var url = "/data/home/home.json";
+            var params = "";
+            
+            /* params accordion */
+            var id_container = "#home-accordion";
+            var tagHeader = "h2";
+            var topOffset = true;
+            
+            var id_section1 = "#section-home-talento";
+            var id_section2 = "#section-home-contactos";
+            
+            /* render handlebars */
+            inventMx.page.wrapper_site.hide().load("app/inventmx/home/home.html", function () {
+                inventMx.ajax.getAjax(url, params, function(resp) {
+                    if (resp){
+                        //$("#page").fadeIn(2000);
+                        data = resp.data.sections[0].items;
+                        dataVideos = data.slice(0, 15);
+                        var redVideos = $("#template-home-red-videos").html();
+                        var tpl_redVideos = Handlebars.compile(redVideos);
+                        var view_redVideos = tpl_redVideos(dataVideos);
+                        $("#home-red-videos").html(view_redVideos);
+                        //inventMx.utilities.loaderHide();
+                    } else {
+                        inventMx.utilities.loaderHide();
+                        $("#home-red-videos").append("<p>No hay datos para mostrar</p>");
+                    }
+                    setTimeout(inventMx.utilities.loaderHide, 7000);
+                });
+                                
+                inventMx.utilities.validateAcordeon(id_container,tagHeader,topOffset);
+                inventMx.utilities.homeAddRemoveSections(id_section1,id_section2);
+                
+                $(window).resize(function (e) {
+                    //console.log(e);
+                    e.stopPropagation();
+                    if(inventMx.utilities.accordionStatus != "active"){
+                        inventMx.utilities.validateAcordeon(id_container,tagHeader,topOffset);
+                    }                    
+                    inventMx.utilities.homeAddRemoveSections(id_section1,id_section2);
+                });
+                
+                inventMx.page.wrapper_site.show("fast");
+                return this;
+            });
+            
+        },        
+        acordeon: function(e) {
+            e.preventDefault();
+            
+            var id_container = "#home-accordion";
+            //section = $("#home-accordion").attr("id");
+            inventMx.utilities.validateAcordeonOffsetStatus = true;
+            inventMx.utilities.validateAcordeonOffset(id_container);
+            
+            $(window).resize(function () {
+                //console.log("vt");
+                //console.log(inventMx.utilities.validateAcordeonOffsetStatus);
+                if(inventMx.utilities.validateAcordeonOffsetStatus){
+                    inventMx.utilities.validateAcordeonOffset(id_container);
+                }                
+            });
+            
+            /*deviceWidthWindow = inventMx.utilities.deviceWidthWindow();
+            if (deviceWidthWindow <= 700) {
+                offset1 = $("#home-accordion").offset();
+                var offset = offset1.top;
+                inventMx.utilities.topOffset(offset);
+            }*/
+        },
+        events: {
+            "click #home-accordion h2": "acordeon",
+        },
+        
+    });
+
+});
