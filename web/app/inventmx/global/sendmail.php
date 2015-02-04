d82bdfb345c7bb284f06ea05f6adac7005e86e54
Pepe <joseangel.cruzhernandez@gmail.com>
2015-02-04 16:13:46 -0600

    rj

--- web/app/inventmx/global/sendmail.php
+++ web/app/inventmx/global/sendmail.php
@@ -21,12 +21,12 @@ $referer=isset($_REQUEST['text-referrer']) ? " Vía ".$_REQUEST['text-referrer']
 if ($type == "afiliate") {
     $type = "Afíliate";
 
-    $to = "sergio@inventmx.com,jose.cruz@inventmx.com,luis@inventmx.com";
+    $to = "'sergio@inventmx.com','jose.cruz@inventmx.com','luis@inventmx.com'";
     //$to="webmaster@inventmx.com,luis@inventmx.com,pedro@inventmx.com, sara@inventmx.com,heberto@inventmx.com";
 } else {
     $type = "Anúnciate";
 
-    $to = "sergio@inventmx.com,jose.cruz@inventmx.com,luis@inventmx.com";
+    $to = "'sergio@inventmx.com','jose.cruz@inventmx.com','luis@inventmx.com'";
     //$to="webmaster@inventmx.com,luis@inventmx.com,pedro@inventmx.com,heberto@inventmx.com,kenneth@inventmx.com";
 }
 
@@ -46,26 +46,12 @@ $mailer = Swift_Mailer::newInstance($transport);
 $message = Swift_Message::newInstance($subject)
   ->setFrom(array('webmaster@inventmx.com' => 'Invent.mx'))
   ->setTo(array('sergio@inventmx.com','jose.cruz@inventmx.com','luis@inventmx.com'))
-  //->setTo(array('receiver@domain.org', 'other@domain.org' => 'A name'))
-  ->setBody("$body")
+  ->setContentType('text/html')
+  ->setBody($body)
   ;
 // Send the message
 $result = $mailer->send($message);
-
-//$correo = filter_var( $correo, FILTER_SANITIZE_EMAIL);
-//    $valid = true;
-//}
-
-//$headers   = array();
-//$headers[] = "Content-type: text/html";
-//$headers[] = "From: Invent.mx <webmaster@inventmx.com>";
-//ini_set('sendmail_from', 'example@YourDomain.com');
-//$cabeceras = implode("\n", $headers);
-   
-
-//mail($to, $subject, $message, $cabeceras);
-
-/*if( $valid ){
+if( $result ){
     $respond = array(
         'respond' => true,
         'text' => 'El correo se ha enviado satisfactoriamente.'
@@ -77,7 +63,7 @@ $result = $mailer->send($message);
         'text' => 'Ha ocurrido un error al enviar el mail, recargue la p&aacute;gina e intente de nuevo por favor.'
     );
     print json_encode( $respond );
-}*/
+}
 
 function sanitaze_string($str = ''){
     $str = strip_tags($str);
