a567232672683c7bdf11ed471cc14ff31a1c406e
Luis Contreras <luis@invent.mx>
2015-05-05 12:37:36 -0500

    Update modules views security breach

--- sites/all/modules/contrib/views/theme/views-view-rss.tpl.php
+++ sites/all/modules/contrib/views/theme/views-view-rss.tpl.php
@@ -6,7 +6,6 @@
  *
  * @ingroup views_templates
  */
-
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
 <rss version="2.0" xml:base="<?php print $link; ?>"<?php print $namespaces; ?>>
