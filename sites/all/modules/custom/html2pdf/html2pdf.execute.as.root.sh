2b4752ab8de9efd97e6cd61744e95cce559f1318
Luis Contreras <luis@invent.mx>
2015-06-17 13:17:53 -0500

    Update core and some modules

--- sites/all/modules/custom/html2pdf/html2pdf.execute.as.root.sh
+++ sites/all/modules/custom/html2pdf/html2pdf.execute.as.root.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Para pdf en Centos, ejecutar como root
+#cd /usr/local/src/
 wget -O /usr/local/src/wkhtmltopdf.tar.bz2 -t 5 http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.9.9-static-i386.tar.bz2
 sleep 1
 tar -jxvf /usr/local/src/wkhtmltopdf.tar.bz2
