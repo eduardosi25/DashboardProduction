4dc86c6dfa9d01dc084ec9f053bd9d38dedeb79e
Angel <jose.cruz@invent.mx>
2015-07-02 11:00:33 -0500

    webApps slider final

--- web/index.php
+++ web/index.php
@@ -92,8 +92,7 @@
         </footer>    
 
         <script type="text/javascript" src="/web/js/libs/inventmx_libs.min.js"></script>
-        <script type="text/javascript" src="/web/js/site/tools.js"></script>
-        <script type="text/javascript" src="/web/js/site/routing.min.js"></script>
+        <script type="text/javascript" src="/web/js/site/tools.min.js"></script>
         
         <script id="metas" type="template/metas">
             <meta name="charset" content="utf-8" />
