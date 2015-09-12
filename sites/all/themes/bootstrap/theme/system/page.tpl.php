e27d3bdafbb151969ecaec07bb8be4ec56dc1bf0
Luis Contreras <luis@invent.mx>
2015-09-11 20:01:52 -0500

    Add sessions to SIPI metrics

--- sites/all/themes/bootstrap/theme/system/page.tpl.php
+++ sites/all/themes/bootstrap/theme/system/page.tpl.php
@@ -80,7 +80,7 @@ if(arg(0)==='sipi'){
   <div class="container">
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+      <a class="logo navbar-btn pull-left" href="/<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
