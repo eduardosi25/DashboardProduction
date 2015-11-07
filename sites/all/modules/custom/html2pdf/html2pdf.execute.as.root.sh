259ee71f27a3d20efbe27867f189b79490fb8a6d
Luis Contreras <luis@invent.mx>
2015-11-06 19:10:17 -0600

    Fix html2pdf

--- sites/all/modules/custom/html2pdf/html2pdf.execute.as.root.sh
+++ sites/all/modules/custom/html2pdf/html2pdf.execute.as.root.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #Para pdf en Centos, ejecutar como root
-#cd /usr/local/src/
-wget -O /usr/local/src/wkhtmltopdf.tar.bz2 -t 5 http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.9.9-static-i386.tar.bz2
+#cd ~
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm
 sleep 1
-tar -jxvf /usr/local/src/wkhtmltopdf.tar.bz2
-sleep 1
-mv /usr/local/src/wkhtmltopdf-i386 /usr/local/src/wkhtmltopdf
-ln -s /usr/local/src/wkhtmltopdf /usr/local/bin/wkhtmltopdf
-sleep 1
+yum localinstall -y wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm
 yum -y install libXext libXext-devel
