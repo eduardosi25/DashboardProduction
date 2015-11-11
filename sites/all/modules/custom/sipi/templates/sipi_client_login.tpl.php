d3af5fdf36caadeb95d469f3f88a658362b7996b
Luis Contreras <luis@invent.mx>
2015-11-10 21:47:08 -0600

    Ajustes de invent.mx para codiga

--- sites/all/modules/custom/sipi/templates/sipi_client_login.tpl.php
+++ sites/all/modules/custom/sipi/templates/sipi_client_login.tpl.php
@@ -1,4 +1,4 @@
-<ul class="menu nav right">
+<ul class="menu nav right" style="position: absolute; right: 0px;">
   <li class="first leaf sipi-login-name">Hola <?php print $data->name?></li>
   <li class="last leaf"><a href="/user/logout"><?php print(t('Log out'));?></a></li>
 </ul>
