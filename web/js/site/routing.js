f4c113bd401d3b28e061c509f79d903d43829bd6
Angel De la Cruz <jose.cruz@invent.mx>
2016-04-04 09:43:39 -0500

    webapps invent | routing

--- web/js/site/routing.js
+++ web/js/site/routing.js
@@ -1,50 +1,48 @@
 $(function() {
-
     var AppRoutes = Backbone.Router.extend({
         routes: {
             "": "home",
             "/": "home",
+            "la-red-invent": "laRedInvent",
+            "content": "content",
+            "media-happenings": "mediaHappenings",
+            "network-ads": "networkAds",
+            "blogs": "blogs",
+            "contacto": "contacto",
+            
             "aviso-de-privacidad": "avisoPrivacidad",
             "politica-ambiental": "politicaAmbiental",
-            "audiencias-y-contenidos": "audienciasContenidos",
-            "audiencias-y-contenidos/:idSection": "audienciasContenidos",
-            "marketers": "marketers",
-            "marketers/:idSection": "marketers",
-            "red-de-video": "redeVideo",
-            "casos-de-exito": "casoExito",
             "afiliate": "afiliate",
             "anunciate": "anunciate",
-            ":nameTalento": "perfilTalento",
-            ":nameSite": "perfilSitio"
         },
         home: function() {
             inventMx.home.vista = new inventMx.home.Home();
         },
-        audienciasContenidos: function(idSection) {
-            inventMx.home.pgAudienciasContenidos = new inventMx.home.HomeAudienciasContenidos(idSection);
+        laRedInvent: function() {
+            console.log("laRedInvent");
         },
-        marketers: function(idSection) {
-            
-            inventMx.home.pgMarketers = new inventMx.home.homeMarketers(idSection);
+        content: function() {
+            console.log("content");
         },
-        redeVideo: function() {       
-            inventMx.home.pgRedeVideo = new inventMx.home.homeRedeVideo();
+        mediaHappenings: function() {
+            console.log("mediaHappenings");
         },
-        casoExito: function() {           
-            inventMx.home.pgCasoExito = new inventMx.home.homeCasoExito();
+        networkAds: function() {
+            console.log("networkAds");
         },
-        afiliate: function() { 
+        blogs: function() {
+            console.log("blogs");
+        },
+        contacto: function() {
+            inventMx.home.pHomes = new inventMx.home.pHomes();
+        },
+        
+        afiliate: function() {
             inventMx.home.pgAfiliate = new inventMx.home.homeAfiliate();
         },
         anunciate: function() {
             inventMx.home.pgAnunciate = new inventMx.home.homeAnunciate();
-        },
-        perfilTalento: function(nameTalento) {  
-            inventMx.home.pgPerfilTalento = new inventMx.home.homePerfilTalento(nameTalento);
-        },
-        perfilSitio: function(nameSite) {
-            inventMx.home.pgPerfilSitio = new inventMx.home.homePerfilSitio(nameSite);
-        },        
+        },    
         avisoPrivacidad: function() {
             inventMx.home.pgAvisoPrivacidad = new inventMx.home.avisoPrivacidad();
         },        
