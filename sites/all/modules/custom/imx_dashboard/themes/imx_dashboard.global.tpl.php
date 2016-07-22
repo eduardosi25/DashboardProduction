b2a14dd3dd179a0152e81bc7b5c72f4b58bc0056
Luis Contreras <luis@inventmx.com>
2016-07-21 21:27:07 -0500

    Fix dashboard

--- sites/all/modules/custom/imx_dashboard/themes/imx_dashboard.global.tpl.php
+++ sites/all/modules/custom/imx_dashboard/themes/imx_dashboard.global.tpl.php
@@ -11,19 +11,15 @@
     fjs.parentNode.insertBefore(js,fjs);js.onload=function(){g.load('analytics')};
   }(window,document,'script'));
 </script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <!-- Step 2: Load the components and their dependencies. -->
-<!-- Include the ViewSelector2 component script. -->
-<script src="/sites/all/modules/custom/imx_dashboard/js/view-selector2.js"></script>
-<!-- Include the DateRangeSelector component script. -->
-<script src="/sites/all/modules/custom/imx_dashboard/js/date-range-selector.js"></script>
-<!-- Include the ActiveUsers component script. -->
-<script src="/sites/all/modules/custom/imx_dashboard/js/active-users.js"></script>
+<script src="/sites/all/modules/custom/imx_dashboard/js/active-users.min.js"></script>
 <!-- Include the CSS that styles the charts. -->
 <link rel="stylesheet" href="/sites/all/modules/custom/imx_dashboard/css/imx_dashboard.css">
 </head>
 <body>
   <!-- Step 3: Add HTML containers to host the dashboard components.. -->
-  <div id="embed-api-auth-container"></div>
+  <div id="embed-api-auth-container" class="invent"></div>
   <?php
   $index=1;
   foreach($data['views'] as $sitename => $view):
@@ -42,6 +38,7 @@
 <script>
 gapi.analytics.ready(function() {
   gapi.analytics.auth.authorize({
+    userInfoLabel : 'Google Analytics con la cuenta de: ',
     container: 'embed-api-auth-container',
     clientid: '393479470712-nhtkfs438r5u6tcprkd9bmlppr75b5a4.apps.googleusercontent.com'
   });
@@ -52,9 +49,9 @@ gapi.analytics.ready(function() {
   ?>
   var activeUsers_<?php print($sitename);?> = new gapi.analytics.ext.ActiveUsers({
     container: 'active-users-<?php print($sitename);?>',
-    pollingInterval: <?php print(rand(15,45));?>
+    pollingInterval: <?php print(rand(15,30));?>
   });
-  activeUsers_<?php print($sitename);?>.set({ids:'<?php print($view);?>'}).execute();
+  activeUsers_<?php print($sitename);?>.set({ids:'<?php print($view);?>'});
   activeUsers_<?php print($sitename);?>.once('success', function() {
     var element = this.container.firstChild;
     var timeout;
@@ -69,6 +66,25 @@ gapi.analytics.ready(function() {
       }, 3000);
     });
   });
+  <?php
+  if($index>5 && $index<10){
+  ?>
+  window.setTimeout(function(){
+    activeUsers_<?php print($sitename);?>.execute();
+  },'1000');
+  <?php
+  }else if($index>10){
+  ?>
+  window.setTimeout(function(){
+    activeUsers_<?php print($sitename);?>.execute();
+  },'2000');
+  <?php
+  }else{
+  ?>
+  activeUsers_<?php print($sitename);?>.execute();
+  <?php
+  }
+  ?>
   <?php
   $index++;
   endforeach;
