4900a11044e027dc40adb3e5ed2fa180c1cb346b
Luis Contreras <luis@inventmx.com>
2017-11-08 17:42:27 -0600

    Add atom namespace

--- sites/all/themes/bootstrap/templates/views-view-rss--play-feeds--feed.tpl.php
+++ sites/all/themes/bootstrap/templates/views-view-rss--play-feeds--feed.tpl.php
@@ -13,7 +13,7 @@
 $channel=taxonomy_term_load(arg(2));
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>\n"; ?>
-<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xmlns:mi="http://schemas.ingestion.microsoft.com/common/" version="2.0">
+<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xmlns:mi="http://schemas.ingestion.microsoft.com/common/" version="2.0">
   <channel>
     <title><?php print $channel->name; ?></title>
     <link><?php print isset($channel->field_link['und'][0]['value']) && !empty($channel->field_link['und'][0]['value']) ? $channel->field_link['und'][0]['value'] : ''; ?></link>
